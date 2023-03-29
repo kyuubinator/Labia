9206793c5c197d73ef57a68cde4af332b4617050
RAFA\Rafael <rafae>
2023-03-29 21:52:36 +0100

    Sounds implemented on Andre scene

--- Labia/Assets/Scripts/MainMenu/UiManagerMain.cs
+++ Labia/Assets/Scripts/MainMenu/UiManagerMain.cs
@@ -27,9 +27,12 @@ public class UiManagerMain : MonoBehaviour
 
     private void Start()
     {
-        //ISTO TA MAl
-        sliderVFXVolume.value = SoundManager.instance.SliderVFXVolume.value;
-        sliderMusicVolume.value = SoundManager.instance.SliderMusicVolume.value;
+        if(SoundManager.instance.SliderMusicVolume)
+        {
+            sliderVFXVolume.value = SoundManager.instance.SliderVFXVolume.value;
+            sliderMusicVolume.value = SoundManager.instance.SliderMusicVolume.value;
+
+        }
         SoundManager.instance.PlayBackGroundMusic(backGroundMusic);
         SoundManager.instance.SetVolumeSliders(sliderMusicVolume, sliderVFXVolume);
     }
@@ -38,14 +41,14 @@ public class UiManagerMain : MonoBehaviour
     {
         DeactivateMenus();
         mainMenu.SetActive(value);
-        //SoundManager.instance.PlayVFXSound(buttonClickClip[Random.Range(0, buttonClickClip.Length)]);
+        SoundManager.instance.PlayVFXSound(buttonClickClip[Random.Range(0, buttonClickClip.Length)]);
 
     }
 
     public void PauseMenuSwap(bool value)
     {
         pauseMenu.SetActive(value);
-        //SoundManager.instance.PlayVFXSound(openPauseMenu[Random.Range(0, openPauseMenu.Length)]);
+        SoundManager.instance.PlayVFXSound(openPauseMenu[Random.Range(0, openPauseMenu.Length)]);
         if (value) 
         {
             animMain.SetTrigger("ClickPause");
@@ -61,7 +64,7 @@ public class UiManagerMain : MonoBehaviour
     IEnumerator DelayGameChoice(bool value)
     {
         animMain.SetTrigger("ClickPlay");
-        //SoundManager.instance.PlayVFXSound(buttonClickClip[Random.Range(0, buttonClickClip.Length)]);
+        SoundManager.instance.PlayVFXSound(buttonClickClip[Random.Range(0, buttonClickClip.Length)]);
         yield return new WaitForSeconds(0.5f);
         DeactivateMenus();
         gameChoiceMenu.SetActive(value);
@@ -69,8 +72,8 @@ public class UiManagerMain : MonoBehaviour
 
     public void TutorialButton()
     {
-        //SoundManager.instance.PlayVFXSound(buttonClickClip[Random.Range(0, buttonClickClip.Length)]);
-        ////tutorial.Play();
+        SoundManager.instance.PlayVFXSound(buttonClickClip[Random.Range(0, buttonClickClip.Length)]);
+        //tutorial.Play();
         animMain.SetTrigger("ClickTutorial");
         animGame.SetTrigger("ClickTutorial");
     }
@@ -89,7 +92,7 @@ public class UiManagerMain : MonoBehaviour
 
     IEnumerator DelayGameMode(int value)
     {
-        //SoundManager.instance.PlayVFXSound(buttonClickClip[Random.Range(0, buttonClickClip.Length)]);
+        SoundManager.instance.PlayVFXSound(buttonClickClip[Random.Range(0, buttonClickClip.Length)]);
         yield return new WaitForSeconds(0.5f);
         gameSelected[value].SetActive(true);
     }
